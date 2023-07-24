5d08a0c1cf5e4403729b0b7851611f5709cbaced
unknown <quocvietqng02@gmail.com>
2023-07-24 07:49:54 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { getPokemonList, getPokemonDescription, getPokemonSpriteUrl } from './helper';
+import { useEffect, useState, useRef } from 'react';
 function App() {
+  const [pokemonList, setPokemonList]= useState([])
+  const [selectedIndex, setSelectedIndex]= useState(0)
+  const [des, setDes]= useState('')
+  const ref= useRef(null)
+  useEffect( ()=> {
+     getPokemonList().then(list=>{ setPokemonList(list); console.log(list)})
+    
+  }, [])
+  //fetch the selected pokemon
+  useEffect(()=> {
+    getPokemonDescription(selectedIndex+1).then((des)=> setDes(des))
+
+
+  }, [selectedIndex])
+  const handleChange= (e)=> {
+    console.log(e.target.value)
+    console.log(e.target.selectedIndex)
+    setSelectedIndex(e.target.selectedIndex)
+  }
+  const prevBtnClick= (e)=> {
+    let index= selectedIndex
+    if (index==0) {
+      index=pokemonList.length-1
+      
+    } else {
+     index-= 1
+    }
+    ref.current.selectedIndex = index
+    setSelectedIndex(index)
+    console.log(selectedIndex)
+  }
+  const nextBtnClick= (e)=> {
+    let index= selectedIndex
+    if (index==pokemonList.length-1) {
+      index=0
+      
+    } else {
+     index+= 1
+    }
+    ref.current.selectedIndex = index
+    setSelectedIndex(index)
+    console.log(selectedIndex)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div classname="select">
+      <select onChange={handleChange} ref={ref}>
+        {pokemonList.map((pokemon, id)=> <option key={id} >{pokemon.name}</option> )}
+      </select>
+     </div>
+     <div className='description'>
+      <p>{des}</p>
+      <img
+       src={getPokemonSpriteUrl(selectedIndex+1)}
+       width="200"
+       height="200"
+       style={{borderRadius: '50%',backgroundColor: 'red'}}
+       
+       />
+     </div>
+     <div className='btn-group'>
+      <button
+      onClick={prevBtnClick}
+      >Previous</button>
+      <button
+      onClick={nextBtnClick}>Next</button>
+     </div>
     </div>
   );
 }
--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,5 @@
 export async function getPokemonList() {
-    const data= awaitfetch(
+    const data= await fetch(
         'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150'
     ).then(res=> res.json())
     return data.results;
@@ -7,5 +7,13 @@ export async function getPokemonList() {
 export async function getPokemonDescription(index) {
 const pokemon= await fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`)
 .then(res=> res.json())
-return pokemon.flavor_text_entries[0].flavor_text.replace()
+return pokemon.flavor_text_entries[0].flavor_text.replace(/[\n\f]/, '')
+
 }
+export function getPokemonSpriteUrl(index) {
+    return  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index}.png`
+}
+
+
+
+
